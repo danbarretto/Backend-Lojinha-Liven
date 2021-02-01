52e9c61fb1586d8aa35e2afe5192e49299966194
Daniel Sá Barretto <danielbarretto@usp.br>
2021-02-01 12:04:48 -0300

    Add user migrations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import express, {Request, Response} from 'express'
+import Knex from 'knex'
+import {Model} from 'objection'
+import bodyParser from 'body-parser'
+const config = require('./knexfile.js') 
+import 'dotenv/config'
 
+
+const knex = Knex(config.development)
 const app = express()
+
+app.use(bodyParser.json())
 
 app.get('/', (req: Request, res:Response)=>{
     res.send('Hello lojinha!')
